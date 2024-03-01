d043f868526cf1899fa2655b9382354212111711
chenchen13 <chenchen13@kuaishou.com>
2024-03-01 17:45:15 +0800

    format use clang-format

--- unittest/unittest_grammar.cpp
+++ unittest/unittest_grammar.cpp
@@ -4,4 +4,9 @@
 #include "fmt/core.h"
 #include "gtest/gtest.h"
 
-TEST(grammar, decltype_case) { fmt::print("the magic mode is {}", 143); }
+TEST(grammar, decltype_case) {
+  fmt::print("the magic mode is {}", 143);
+  auto v = 123;
+  static_assert(std::is_same_v<decltype(v), int>);
+  static_assert(std::is_same_v<decltype(v), int32_t>);
+}
